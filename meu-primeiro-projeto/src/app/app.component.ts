968b87899ad24a42dc3307165de463a8d017200d
Marconcilio Souza <marconcilioka@kalunga.com.br>
2025-05-29 15:50:11 -0300

    Property binding

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new/new.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    NewComponent, 
+    TemplateBindingComponent, 
+    TemplateVariablesComponent, 
+    TemplateControlFlowComponent
+  ],
+  template: `
+  <h1>Curso de Angular</h1>
+  <!-- <app-template-binding /> -->
+  <!-- <app-template-variables /> -->
+   <app-template-control-flow />
+  `,
 })
 export class AppComponent {
-  title = 'meu-primeiro-projeto';
+  title = 'Meu Primeiro Projeto';
 }
---
+++ meu-primeiro-projeto/src/app/components/template/template-binding/template-binding.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-binding',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+  public name = 'Dener Troquatte';
+  public age = 16;
+  public condition = this.age > 18 ? "Maior de idade" : "Menor de idade";
+  public isDisable = true;
+  public srcValue = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg63mY5s7XOPcvkUQInmzLxbp5xto1HqHo0V2wkn7Vx7LXwnGnH2WIHApQiXt7fy9qknBzRDPWB-SOq2tCMbFSeqpozkE1weh1emiE06QybhgeAaVS0V6cVnObCpCLuiKccmAmXxAmrmmA/s1600/cocoroca.jpg";
+  public isTextDecoration = this.age > 18 ? 'underline' : 'none';
+  constructor() {
+    setTimeout(() => {
+      this.name = "João e Maria";
+    }, 1000);
+  }
+  public increment_age() {
+    return this.age++;
+  }
+  public decrement_age() {
+    return this.age--;
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event);
+  }
+
+  public onMouseMove(event: MouseEvent) {
+    return console.log({
+      clientX: event.clientX,
+      clientY: event.clientY,
+    });
+  }
+}
