e9c5b8407b77954ada0101d88606f7b7042e7578
Marconcilio Souza <marconcilioka@kalunga.com.br>
2025-05-30 13:59:16 -0300

    template defer

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -5,23 +5,26 @@ import { NewComponent } from './components/new/new.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewComponent } from "./components/template/template-deferrable-view/template-deferrable-view.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
-    RouterOutlet, 
-    NewComponent, 
-    TemplateBindingComponent, 
-    TemplateVariablesComponent, 
-    TemplateControlFlowComponent
-  ],
+    CommonModule,
+    RouterOutlet,
+    NewComponent,
+    TemplateBindingComponent,
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewComponent
+],
   template: `
   <h1>Curso de Angular</h1>
   <!-- <app-template-binding /> -->
   <!-- <app-template-variables /> -->
-   <app-template-control-flow />
+   <!-- <app-template-control-flow /> -->
+   <app-template-deferrable-view />
   `,
 })
 export class AppComponent {
