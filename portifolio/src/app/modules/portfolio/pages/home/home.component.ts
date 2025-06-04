6eb8b64dbd71346e54ec2abf35adddc11aa6c876
Marconcilio Souza <marconcilioka@kalunga.com.br>
2025-06-04 17:42:44 -0300

    continuação portfolio

--- portifolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ portifolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HearderComponent } from '../../components/hearder/hearder.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
+import { ExperiencesComponent } from '../../components/experiences/experiences.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HearderComponent, KnowledgeComponent],
+  imports: [HearderComponent, KnowledgeComponent, ExperiencesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
